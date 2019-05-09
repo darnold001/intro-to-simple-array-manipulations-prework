d0307599366253c8d6a1ea2d523ee0a466f3cbd4
Devin Arnold <dbarnold321@gmail.com>
2019-05-09 02:16:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,6 @@ def using_push (array, string)
   array.push (string)
 end
 
-def using_unshift ( array, string)
-  array = []
-  array.unshift(string)
+def using_unshift ( array, string) s
+  array.unshift(tring)
 end
